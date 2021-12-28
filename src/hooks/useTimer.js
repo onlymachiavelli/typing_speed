8ca75bfef079d7de23885cd7dc5cfb21b89e5059
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-28 16:53:08 +0100

    timer

--- src/hooks/useTimer.js
+++ src/hooks/useTimer.js
@@ -3,11 +3,20 @@ import React, { useState, useEffect } from 'react'
 
 
 const useTimer = () => {
-    const [time, setTime] = useState()
+    const [time, setTime] = useState(60)
+    const [launch, setLaunch] = useState(false)
 
     useEffect(() => {
-
-    }, [])
+        time == 0 ? setLaunch(false) : setLaunch(launch)
+        time == 0 ? setTime(60) : setTime(time)
+        if (launch) {
+            let timer = setInterval(() => {
+                setTime(time - 1)
+            }, 1000)
+        }
+        return () => clearInterval(timer)
+    }, [time, launch])
+    return { time, launch, setTime, setLaunch }
 }
 
 
