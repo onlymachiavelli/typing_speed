0205438b1449ee0a03747cc02286ac9f45ec69bc
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-28 16:44:48 +0100

    usetimer

---
+++ src/hooks/useTimer.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+
+
+
+const useTimer = () => {
+    const [time, setTime] = useState()
+
+    useEffect(() => {
+
+    }, [])
+}
+
+
+export default useTimer 
